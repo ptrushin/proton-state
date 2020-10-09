7a7a2c2921670b1899a81a68a1ed2d0a6174a5fe
Павел Трушин <ptrushin@custis.ru>
2020-10-09 18:05:44 +0300

    -

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import locale from 'antd/es/locale/en_US'
 //import locale from 'antd/es/locale/ru_RU';
 //import 'moment/locale/ru';
 import 'antd/dist/antd.css';
-import AntdTagFilterPanelExample from './examples/AntdTagFilterPanelExample'
 import AgGridExample from './examples/AgGridExample'
 
 function App() {
     return <BrowserRouter>
         <ConfigProvider locale={locale}>
             <Route exact path='/' render={(props) => <AgGridExample {...props} />} />
-            <Route path='/AntdTagFilterPanelExample' render={(props) => <AntdTagFilterPanelExample {...props} />} />
             <Route path='/AgGridExample' render={(props) => <AgGridExample {...props} />} />
         </ConfigProvider>
     </BrowserRouter>
