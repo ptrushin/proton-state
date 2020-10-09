207d547d05c7e57bd4b8c3aa388f1b360192fd28
Павел Трушин <ptrushin@custis.ru>
2020-10-09 18:13:47 +0300

    -

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,9 @@
 import ProtonState from "./core/ProtonState";
 import AgGridStateProvider from "./ag-grid/AgGridStateProvider";
+import FilterPanel from "./antd-tag-filter-panel/FilterPanel";
+import AntTagFilterPanelStateProvider from "./antd-tag-filter-panel/AntTagFilterPanelStateProvider";
 
 export { ProtonState }
 export { AgGridStateProvider }
+export { FilterPanel }
+export { AntTagFilterPanelStateProvider }
